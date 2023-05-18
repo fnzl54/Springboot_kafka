4369394310ee078f6ff85ed59c6f6b553b665ba2
fnzl54 <rnjscksdud1@naver.com>
2023-05-18 23:13:47 +0900

    feat: api 요청을 받아서 producer로 넘겨주는 KafkaController

---
+++ src/main/java/com/kafka/demo/controller/KafkaController.java
@@ -0,0 +1,23 @@
+package com.kafka.demo.controller;
+
+import com.kafka.demo.service.KafkaProducer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+  private final KafkaProducer producer;
+
+  @PostMapping
+  public String sendMessage(@RequestParam("message") String message) {
+    this.producer.sendMessage(message);
+
+    return "success";
+  }
+}
