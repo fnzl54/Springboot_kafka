dc8a25cd081ad94366ea218597a8576ac74a3110
fnzl54 <rnjscksdud1@naver.com>
2023-05-21 23:45:15 +0900

    refactor: KafkaProducer 수정

--- src/main/java/com/kafka/demo/service/KafkaProducer.java
+++ src/main/java/com/kafka/demo/service/KafkaProducer.java
@@ -1,7 +1,7 @@
 package com.kafka.demo.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
