4fc2f74183768dd0214505b57a06c441a5b82bf9
fnzl54 <rnjscksdud1@naver.com>
2023-06-08 21:21:00 +0900

    feat: 위도와 경도를 통해 원하는 지역의 날씨 데이터를 반환하는 api 추가

--- src/main/java/com/kafka/demo/service/WeatherService.java
+++ src/main/java/com/kafka/demo/service/WeatherService.java
@@ -19,6 +19,30 @@ public class WeatherService {
   private static final String API_WEATHER_URL = "https://api.openweathermap.org/data/2.5/forecast";
   private static final String API_LOCATION_URL = "http://api.openweathermap.org/geo/1.0/direct";
 
+  public String getWeatherData(String location) throws Exception {
+
+    List<Double> latLon = getLatLonData(location);
+
+    RestTemplate restTemplate = new RestTemplate();
+    String jsonResponse = restTemplate.getForObject(
+        API_WEATHER_URL + "?lat=" + latLon.get(0) + "&lon=" + latLon.get(1) + "&appid=" + API_KEY, String.class);
+
+    JSONObject jsonObject = new JSONObject(jsonResponse);
+    JSONArray listArray = jsonObject.getJSONArray("list");
+
+    for (int i = 0; i < listArray.length(); i++) {
+      JSONObject listItem = listArray.getJSONObject(i);
+      String date = listItem.getString("dt_txt");
+
+      JSONArray weatherArray = listItem.getJSONArray("weather");
+      for (int j = 0; j < weatherArray.length(); j++) {
+        JSONObject weatherItem = weatherArray.getJSONObject(j);
+        System.out.println("Weather " + date + " : " + weatherItem.getString("main"));
+      }
+    }
+
+    return "true";
+  }
 
   public List<Double> getLatLonData(String location) throws Exception {
 
