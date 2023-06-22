e9a33a8cd6fa19122dc9a7cf71885407d438193f
fnzl54 <rnjscksdud1@naver.com>
2023-06-22 20:51:03 +0900

    feat: kakfa 소비 후 출력에 소비하는 시간 측정 코드 추가

--- src/main/java/com/kafka/demo/service/KafkaConsumer.java
+++ src/main/java/com/kafka/demo/service/KafkaConsumer.java
@@ -8,8 +8,13 @@ import java.io.IOException;
 @Service
 public class KafkaConsumer {
 
-  @KafkaListener(topics = "seoul", groupId = "kafka-demo")
+  @KafkaListener(topics = "Rain", groupId = "kafka-demo")
   public void consume(String message) throws IOException {
-    System.out.println(String.format("Consumed message : %s", message));
+    long startTime = System.nanoTime();
+    System.out.println("Consumed message : " + message );
+    long endTime = System.nanoTime();
+    long processingTime = endTime - startTime;
+    double processingTimeMs = (double) processingTime / 1_000_000;
+    System.out.println("메시지 소비 시간: " + processingTimeMs + "ms");
   }
 }
