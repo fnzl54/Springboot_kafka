1686090fa16ec34def4fb9a0f2b10e04e05a334d
fnzl54 <rnjscksdud1@naver.com>
2023-06-22 20:49:25 +0900

    feat: kafka를 사용하는 api와 kafka를 사용하지 않은 api를 구분하여 작성

--- src/main/java/com/kafka/demo/controller/WeatherController.java
+++ src/main/java/com/kafka/demo/controller/WeatherController.java
@@ -1,6 +1,9 @@
 package com.kafka.demo.controller;
 
+import com.kafka.demo.service.KafkaProducer;
 import com.kafka.demo.service.WeatherService;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,15 +13,42 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 public class WeatherController {
+  private final KafkaProducer producer;
   private final WeatherService weatherService;
 
-  @GetMapping("/weather/{location}")
-  public String getWeather(@PathVariable String location) {
+  @GetMapping("/weather1/{location}")
+  public void getWeatherWithKafka(@PathVariable String location) {
+    List<String> weatherData = new ArrayList<>();
+
     try {
-      String weatherData = weatherService.getWeatherData(location);
-      return weatherData;
+      weatherData = weatherService.getWeatherData(location);
     } catch (Exception e) {
-      return "날씨 데이터를 가져오는 중에 오류가 발생했습니다.";
+      System.out.println("날씨 데이터를 가져오는 중에 오류가 발생했습니다.");
+    }
+
+    for (String weatherDataForTime : weatherData) {
+      String[] regionalTimeWeather = weatherDataForTime.split("/");
+      this.producer.sendMessage(regionalTimeWeather[0], regionalTimeWeather[1]);
     }
   }
+
+  @GetMapping("/weather2/{location}")
+  public void getWeatherWithoutKafka(@PathVariable String location) {
+    List<String> weatherData = new ArrayList<>();
+
+    try {
+      weatherData = weatherService.getWeatherData(location);
+    } catch (Exception e) {
+      System.out.println("날씨 데이터를 가져오는 중에 오류가 발생했습니다.");
+    }
+
+    for (String weatherDataForTime : weatherData) {
+      String[] regionalTimeWeather = weatherDataForTime.split("/");
+      if (regionalTimeWeather[0].equals("Rain")) {
+        System.out.println("Consumed message : " + regionalTimeWeather[1] );
+      }
+    }
+
+  }
+
 }
